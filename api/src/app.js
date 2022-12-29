f4b9fd2fae3d6afb12dc3628f561e11720bb35d3
Paul C <pecardozo17@gmail.com>
2022-12-29 13:09:55 -0300

    Fix some characters in the app.js and index.js files

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ const server = express();
 server.name = "API";
 
 server.use(bodyParser.json({ limit: "50mb" }));
-server.use(bodyParser.urlencoded({ extended: true, limit: "500mb"}));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb"}));
 server.use(express.json());
 server.use(cookieParser());
 server.use(morgan("dev"));
